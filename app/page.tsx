dcb67a88ab354dc8424e7a674dfefd595eafd377
Nilesh <akhilrana7500@gmail.com>
2025-06-24 16:17:26 +0530

    using shadcn for theme mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <div className="m-3 flex gap-3">
+        <ModeToggle></ModeToggle>
+        <Button className="bg">Click me</Button>
+      </div>
+
     </div>
   );
 }
