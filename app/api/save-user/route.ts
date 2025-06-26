fa069840c5a41ac32e943600d89c47cf315cbd37
Nilesh <akhilrana7500@gmail.com>
2025-06-26 14:54:04 +0530

    ok

--- app/api/save-user/route.ts
+++ app/api/save-user/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@/lib/generated/prisma';
+import { currentUser } from '@clerk/nextjs/server';
 
 export async function POST(req: NextRequest) {
+  const user = await currentUser();
   const prisma = new PrismaClient()
   const body = await req.json()
   // check if user already exist :
@@ -14,13 +16,15 @@ export async function POST(req: NextRequest) {
       data: {
         name: body.name,
         email: body.email,
+        // picture: user.imageUrl,
+        credits: 5000,
       },
     });
   }
   else {
     console.log("user already exist !")
   }
-  
+
   return NextResponse.json({ message: 'User data received at backend' })
 }
 
