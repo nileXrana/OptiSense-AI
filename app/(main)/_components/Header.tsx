5a4c3bc2dae3044cb299639a981f7d5736da7c36
Nilesh <akhilrana7500@gmail.com>
2025-06-28 23:17:26 +0530

    more....

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -1,9 +1,16 @@
+"use client"
 import React from 'react'
+import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
-    <div>
-      HEADER
+    <div className='border-2 border-white p-3 px-7 shadow-sm flex justify-between items-center'>
+      <Image src={"/botlogo.png"} alt="Logo" width={50} height={50}>
+      </Image>
+      <div className='scale-120 transform hover:scale-150'>
+        <UserButton />
+      </div>
     </div>
   )
 }
--- app/(main)/ai-assistants/page.tsx
+++ app/(main)/ai-assistants/page.tsx
@@ -1,9 +1,57 @@
+'use client'
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import AiAssistantsList from '@/services/AiAssistantsList'
+import Image from 'next/image'
+import { Checkbox } from "@/components/ui/checkbox"
+import { useState } from 'react'
+
+export type ASSISTANT = {
+  id: number,
+  name: string,
+  title: string,
+  image: string,
+  instruction: string,
+  userInstruction: string,
+  sampleQuestions: string[]
+}
 
 const page = () => {
+  const [selectedAssistants, setselectedAssistants] = useState<ASSISTANT[]>([]);
+  const onselect = (obj: ASSISTANT) => {
+    const item = selectedAssistants.find((object: ASSISTANT) => object.id == obj.id);
+    if(item){
+      setselectedAssistants(selectedAssistants.filter ((item:ASSISTANT)=>item.id!==obj.id))
+      return;
+    }
+    setselectedAssistants(prev => [...prev, obj]);
+  }
+  const IsAssistantSelected = (obj: ASSISTANT)=>{
+    const item = selectedAssistants.find((object: ASSISTANT) => object.id == obj.id);
+    return item?true:false
+  }
+
   return (
-    <div className='bg-green-700'>
-      All AI Assistants will be here !
+    <div className='px-10 mt-20 md:px-28 lg:px-36 xl:px-48'> 
+      <div className='flex items-center justify-between'>
+        <div>
+          <h2 className='text-3xl font-bold'>Welcome to the World of AI Assistants 🤖</h2>
+          <p className='text-xl mt-2'>Choose your AI Campanion to Simplify Your Tasks 🚀</p>
+        </div>
+        <Button>Continue</Button>
+      </div>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-5 mb-10'>
+        {AiAssistantsList.map((obj) => (
+          <div key={obj.id} className='hover:border hover:scale-115 transition-all ease-in-out cursor-pointer' onClick={() => onselect(obj)}>
+            <Checkbox className='absolute m-2' checked={IsAssistantSelected(obj)}/>
+            <Image src={obj.image} alt={obj.title} width={300} height={300} className='rounded-xl w-full h-[200px] object-cover' />
+            <h2 className='text-center font-bold text-lg'>{obj.name}</h2>
+            <h2 className='text-center text-gray-600 dark:text-gray-300'>{obj.title}</h2>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
