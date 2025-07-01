3d2235ef25ed0e5dca781b143e7c63255685b1ef
Nilesh <akhilrana7500@gmail.com>
2025-07-01 13:10:43 +0530

    .

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center h-[70vh] text-2xl'>
+      <p className='hover:text-9xl transition-all duration-300 ease-in-out'>DASHBOARD</p>
+    </div>
+  )
+}
+
+export default page
