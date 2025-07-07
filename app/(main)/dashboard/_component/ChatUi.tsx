2575c9b968d2fc49d5033ee4a760a0c5a336be13
Nilesh <akhilrana7500@gmail.com>
2025-07-07 14:07:48 +0530

    ok

--- app/(main)/dashboard/_component/ChatUi.tsx
+++ app/(main)/dashboard/_component/ChatUi.tsx
@@ -8,8 +8,8 @@ import { useModel } from '@/context/ModelContext'
 import { AssistantContext } from '@/context/AssistantContext'
 
 const ChatUi = () => {
-    
-    
+
+
     const [input, setinput] = useState<string>("")
     const [messages, setMessages] = useState<{ role: string; text: string }[]>([]);
     const { selectedModel } = useModel();
@@ -18,35 +18,38 @@ const ChatUi = () => {
     useEffect(() => {
         setshowEmptyChatState(true)
     }, [selectedAssistant])
-    
+
     const onSendMessage = async () => {
         setshowEmptyChatState(false)
         // add user input to msg array :
         if (!input.trim()) return; // check if msg is empty
-        setMessages((prev) => [...prev, { role:"user", text:input }]);
+        setMessages((prev) => [...prev, { role: "user", text: input }]);
+        setinput("")
         const res = await fetch("/api/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({prompt: input, model: selectedModel})
+            body: JSON.stringify({ prompt: input, model: selectedModel })
         });
-
         if (!res.ok) throw new Error("Failed to post");
         const data = await res.json();
         // add AI response to msg array :
-        setMessages((prev) => [...prev, { role: "ai", text: data.text }]);
         const ans = data.candidates[0].content.parts[0].text;
-        console.log(ans);
+        setMessages((prev) => [...prev, { role: "ai", text: ans }]);
     }
 
     return (
         <div className='p-20 relative h-[90vh] border-2'>
-            {showEmptyChatState?<EmptyChatState/>:
+            {showEmptyChatState ? <EmptyChatState /> :
                 <div>
-                    {input}
+                    {messages.map((msg, idx) => (
+                        <p key={idx} className={msg.role === "user" ? "text-blue-600 text-right" : "text-green-600 text-left"}>
+                            <strong>{msg.role}:</strong> {msg.text}
+                        </p>
+                    ))}
                 </div>
             }
             <div className='flex justify-between py-5 gap-5 absolute bottom-2 left-10 w-[90%]'>
-                <Input placeholder='Start Typing Here.....' className='border-2 border-black'
+                <Input value={input} placeholder='Start Typing Here.....' className='border-2 border-black'
                     onChange={(e) => setinput(e.target.value)}
                     onKeyDown={(e) => {
                         if (e.key === "Enter") {
