8ad0fadc60c5eb6e7e44e7964be003be3964e04f
Nilesh <akhilrana7500@gmail.com>
2025-07-07 13:37:30 +0530

    nic

--- app/(main)/dashboard/_component/ChatUi.tsx
+++ app/(main)/dashboard/_component/ChatUi.tsx
@@ -1,17 +1,25 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import EmptyChatState from './EmptyChatState'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Send } from 'lucide-react'
 import { useModel } from '@/context/ModelContext'
+import { AssistantContext } from '@/context/AssistantContext'
 
 const ChatUi = () => {
-
+    
+    
     const [input, setinput] = useState<string>()
     const { selectedModel } = useModel();
-
+    const [showEmptyChatState, setshowEmptyChatState] = useState(true)
+    const { selectedAssistant, setselectedAssistant } = useContext<any>(AssistantContext)
+    useEffect(() => {
+        setshowEmptyChatState(true)
+    }, [selectedAssistant])
+    
     const onSendMessage = async () => {
+        setshowEmptyChatState(false)
         const res = await fetch("/api/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -26,7 +34,7 @@ const ChatUi = () => {
 
     return (
         <div className='p-20 relative h-[90vh] border-2'>
-            <EmptyChatState />
+            {showEmptyChatState&&<EmptyChatState />}
             <div className='flex justify-between py-5 gap-5 absolute bottom-2 left-10 w-[90%]'>
                 <Input placeholder='Start Typing Here.....' className='border-2 border-black'
                     onChange={(e) => setinput(e.target.value)}
@@ -35,7 +43,7 @@ const ChatUi = () => {
                             onSendMessage()
                         }
                     }} />
-                <Button onClick={onSendMessage}>
+                <Button onClick={onSendMessage} className='cursor-pointer'>
                     <Send />
                 </Button>
             </div>
