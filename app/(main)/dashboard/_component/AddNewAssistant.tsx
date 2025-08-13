b55d20a4641c5fb4c2f9e7af2ecf3cb371830a1e
Nilesh <akhilrana7500@gmail.com>
2025-08-13 18:59:23 +0530

    .

---
+++ app/(main)/dashboard/_component/AddNewAssistant.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import AiAssistantsList from '@/services/AiAssistantsList'
+import { BlurFade } from '@/src/components/magicui/blur-fade'
+import { Checkbox } from '@radix-ui/react-checkbox'
+import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { Assistant } from 'next/font/google'
+import { ASSISTANT } from '../../ai-assistants/page'
+import { number } from 'motion/react'
+import { Textarea } from '@/components/ui/textarea'
+
+const AddNewAssistant = ({ children }: any) => {
+
+    // defaultAssistant :
+    const DEFAULT_ASSISTANT = {
+        image:'/bug-fixer.avif',
+            name:'',
+            title:'',
+            instruction:'',
+            id:0,
+            sampleQuestions:[],
+            userInstruction:'' 
+    }
+    // useState :
+    const [selectedAssistant, setselectedAssistant] = useState<ASSISTANT>(DEFAULT_ASSISTANT)
+
+    const onHandleInputChange = (field: string,value: string)=>{
+        setselectedAssistant((prev:any)=>({
+            ...prev,
+            [field]:value
+        }))
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Add New Assistant</DialogTitle>
+                    <DialogDescription asChild>
+                        <div className='grid grid-cols-3 mt-5'>
+                            <div className='border-r-2'>
+                                <Button variant={'secondary'} size={'sm'} className='w-full mb-2 cursor-pointer hover:scale-108'
+                                onClick={()=> setselectedAssistant(DEFAULT_ASSISTANT)}>+ Create New Assistant</Button>
+                                <div className='overflow-y-auto h-[66vh]'>
+                                    {AiAssistantsList.map((obj, index) => (
+                                        <BlurFade key={obj.id} delay={0.25 + index * 0.1} inView>
+                                            <div key={obj.id} className='hover:border-2 border-purple-400 hover:scale-101 transition-all ease-in-out cursor-pointer flex items-center p-2 ' onClick={()=>setselectedAssistant(obj)}>
+                                                <Image src={obj.image} alt={obj.title} width={40} height={40} className='rounded-lg w-[40px] h-[40px] object-cover' />
+                                                <h2 className='text-xs p-2 text-center text-gray-600 dark:text-gray-300'>{obj.title}</h2>
+                                            </div>
+                                        </BlurFade>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className='col-span-2 ml-3'>
+                                <div className='flex gap-5'>
+                                    {selectedAssistant && <Image src={selectedAssistant?.image} alt='assistant' width={150} height={150} className='w-[100px] h-[100px] rounded-xl cursor-pointer object-cover' 
+                                    />}
+                                    <div className='flex flex-col gap-3'>
+                                        <Input placeholder='Name of Assistant' className='w-full'
+                                        value={selectedAssistant?.name}
+                                         onChange={(event)=>onHandleInputChange('name',event.target.value)}/>
+                                        <Input placeholder='Title of Assistant' className='w-full'
+                                        value={selectedAssistant?.title}
+                                        onChange={(event)=>onHandleInputChange('title',event.target.value)} 
+                                        />
+                                    </div>
+
+                                </div>
+                                <div className='mt-4'>
+
+                                </div>
+                                <div>
+                                    <h2 className='text-gray-500 mb-2'>Instruction:</h2>
+                                    <Textarea placeholder='Add Instruction'
+                                    className='h-[200px]'
+                                    value={selectedAssistant?.userInstruction}
+                                    onChange={(event)=>onHandleInputChange('userInstruction',event.target.value)}
+                                    />
+                                </div>
+
+                                <div className='flex gap-5 justify-end mt-5'>
+                                    <Button>Cancel</Button>
+                                    <Button>Add</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default AddNewAssistant
--- app/(main)/dashboard/_component/AssistantList.tsx
+++ app/(main)/dashboard/_component/AssistantList.tsx
@@ -9,6 +9,7 @@ import { AssistantContext } from '@/context/AssistantContext'
 import { UserButton } from "@clerk/nextjs";
 import { useUser } from '@clerk/nextjs'
 import { BlurFade } from '@/src/components/magicui/blur-fade'
+import AddNewAssistant from './AddNewAssistant'
 
 const AssistantList = () => {
 
@@ -28,20 +29,20 @@ const AssistantList = () => {
   }, [])
 
   useEffect(() => {
-    const fetchData = async ()=>{
-        const res =  await fetch('/api/save-user', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            email: user?.primaryEmailAddress?.emailAddress,
-          })
+    const fetchData = async () => {
+      const res = await fetch('/api/save-user', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: user?.primaryEmailAddress?.emailAddress,
         })
-        const result = await res.json();
-        setUSER(result)
-      }
-      fetchData();
+      })
+      const result = await res.json();
+      setUSER(result)
+    }
+    fetchData();
   }, [user])
-  
+
   // All useState and useContext :
   const [myAssistants, setmyAssistants] = useState([])
   const { selectedAssistant, setselectedAssistant } = useContext<any>(AssistantContext)
@@ -49,42 +50,44 @@ const AssistantList = () => {
   return (
     <div className='p-3 bg-secondary h-screen relative'>
       <BlurFade duration={0.4}>
-      <h1 className='font-bold text-md text-center'>
-        Your Personal AI Assistants
-      </h1>
+        <h1 className='font-bold text-md text-center'>
+          Your Personal AI Assistants
+        </h1>
       </BlurFade>
       <BlurFade duration={0.8}>
-      <Button className='w-full mt-3 cursor-pointer'>
-        Add New Assistant
-      </Button>
+        <AddNewAssistant>
+          <Button className='w-full mt-3 cursor-pointer'>
+            Add New Assistant
+          </Button>
+        </AddNewAssistant>
       </BlurFade>
       <BlurFade duration={1.2}>
-      <Input className='bg-white mt-3 mb-3' placeholder='Search' />
+        <Input className='bg-white mt-3 mb-3' placeholder='Search' />
       </BlurFade>
       <div className='overflow-scroll h-[60%]'>
         {myAssistants.map((assistant: ASSISTANT, index) => (
           <BlurFade key={index} duration={0.5 * index}>
-          <div key={index} className={`p-2 flex gap-3 items-center hover:bg-gray-300 hover:dark:bg-slate-700 cursor-pointer rounded-xl ${assistant.id == selectedAssistant?.id && 'bg-purple-300 hover:bg-purple-300'} `} onClick={() => { setselectedAssistant(assistant) }}>
-            <Image src={assistant.image} alt={assistant.name} width={60} height={60}
-              className='rounded-xl w-[60px] h-[60px] object-cover'>
-            </Image>
-            <div>
-              <h1 className='text-md font-bold'>{assistant.name}</h1>
-              <h1 className='text-sm text-gray-600 dark:text-gray-300'>{assistant.title}</h1>
+            <div key={index} className={`p-2 flex gap-3 items-center hover:bg-gray-300 hover:dark:bg-slate-700 cursor-pointer rounded-xl ${assistant.id == selectedAssistant?.id && 'bg-purple-300 hover:bg-purple-300'} `} onClick={() => { setselectedAssistant(assistant) }}>
+              <Image src={assistant.image} alt={assistant.name} width={60} height={60}
+                className='rounded-xl w-[60px] h-[60px] object-cover'>
+              </Image>
+              <div>
+                <h1 className='text-md font-bold'>{assistant.name}</h1>
+                <h1 className='text-sm text-gray-600 dark:text-gray-300'>{assistant.title}</h1>
+              </div>
             </div>
-          </div>
           </BlurFade>
         ))}
       </div>
       <div className='absolute bottom-22 left-2 flex gap-3 items-center bg-gray-300 px-3 py-2 rounded-2xl w-[92%] cursor-pointer hover:bg-gray-400'>
         <UserButton afterSignOutUrl="/signin" />
         <div className='text-[14px]'>
-        <p className='font-bold'>
-          {USER?.name}
-        </p>
-        <p className='text-gray-600 hover:text-pink-800 cursor-pointer'>
-          {USER?.orderId?"Pro Plan":"Free Plan"}
-        </p>
+          <p className='font-bold'>
+            {USER?.name}
+          </p>
+          <p className='text-gray-600 hover:text-pink-800 cursor-pointer'>
+            {USER?.orderId ? "Pro Plan" : "Free Plan"}
+          </p>
         </div>
       </div>
 
