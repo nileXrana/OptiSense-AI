c33c97a833aeca01a2194dc0a86caee5baf1ecb0
Nilesh <akhilrana7500@gmail.com>
2025-08-21 10:36:07 +0530

    z

--- app/api/selected-assistants/route.ts
+++ app/api/selected-assistants/route.ts
@@ -3,19 +3,19 @@ import { prisma } from '@/lib/prisma';
 import { currentUser } from "@clerk/nextjs/server"
 
 export async function POST(req: NextRequest) {
-   const user = await currentUser()
-   if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
-  
+  const user = await currentUser()
+  if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+
   const body = await req.json()
   for (const obj of body) {
-  const existing = await prisma.userAiAssistants.findFirst({
-    where: {uid: user?.id, name: obj.name},
-  });
-  if (!existing) {
-    await prisma.userAiAssistants.create({
-      data: { ...obj, uid: user?.id },
+    const existing = await prisma.userAiAssistants.findFirst({
+      where: { uid: user?.id, name: obj.name },
     });
+    if (!existing) {
+      await prisma.userAiAssistants.create({
+        data: { ...obj, uid: user?.id },
+      });
+    }
   }
-}
   return NextResponse.json("user assistants are saved in db")
 }
