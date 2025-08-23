14aff9984d1f5a169dc9724136587d5286db7dd2
Nilesh <akhilrana7500@gmail.com>
2025-08-23 11:25:16 +0530

    done responsiveness

--- app/(main)/dashboard/_component/AssistantList.tsx
+++ app/(main)/dashboard/_component/AssistantList.tsx
@@ -336,7 +336,7 @@ const AssistantList = ({ preloadedAssistants = [], onMobileClose, initialUserDat
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </BlurFade>
-      <div className='overflow-scroll h-[60vh] '>
+      <div className='overflow-scroll h-[60vh] max-sm:h-[66vh]'>
         {filteredAssistants.length > 0 ? (
           filteredAssistants.map((assistant: ASSISTANT, index) => (
             <BlurFade key={index} duration={0.3 * index}>
